3907c332a98939039221bf241dd9f497b83be332
wyz <wuyizhe@fang.com>
2019-12-02 08:01:28 +0800

    单向链表 测试类

--- src/main/java/com/fangtianxia/demo/day03/Node.java
+++ src/main/java/com/fangtianxia/demo/day03/Node.java
@@ -10,9 +10,13 @@ import lombok.Data;
 @Data
 public class Node {
 
-    //节点内容
+    /**
+     * 节点内容
+     */
     private int data;
-    //下一个节点
+    /**
+     * 下一个节点
+     */
     private Node next;
 
     public Node(int data) {
@@ -23,30 +27,17 @@ public class Node {
      * 为节点追加节点
      */
     public Node append(Node node) {
-        //当前节点
-        Node currentNode = this;
         //循环往后找
         while(true){
             //取出下一个节点
             Node nextNode = this.next;
-            //如果下一个节点为null，当前节点已经是最后一个节点
+            //如果下一个节点为null，当前节点已经是最后一个节点,将下一个节点（传进来的值）赋值给当前节点的next属性
             if (nextNode==null){
+                this.next=node;
                 break;
             }
-            //如果下一个节点不为null，将下一个节点赋值给当前节点的next属性
-            this.next=node;
         }
-        return currentNode;
-        //也可以返回this
-        //return this;
-
-    }
-
-    /**
-     * 获取下一个节点
-     */
-    public Node next() {
-        return this.next;
+        return next;
     }
 
     /**
--- src/main/java/com/fangtianxia/demo/day03/TestNode.java
+++ src/main/java/com/fangtianxia/demo/day03/TestNode.java
@@ -1,5 +1,9 @@
 package com.fangtianxia.demo.day03;
 
+/**
+ * @author wyz
+ * 单向链表的测试类
+ */
 public class TestNode {
 
     public static void main(String[] args) {
@@ -7,17 +11,16 @@ public class TestNode {
         Node node1 = new Node(1);
         Node node2 = new Node(2);
         Node node3 = new Node(3);
-        Node node4 = new Node(4);
         //追加节点
         node1.append(node2);
         node2.append(node3);
-        node3.append(node4);
 
         //取出下一个节点
         System.out.println(node1.getData());
-        System.out.println(node1.next().getData());
-        System.out.println(node2.next().getData());
-        System.out.println(node3.next().getData());
+        System.out.println(node1.getNext().getData());
+        System.out.println(node3.isLast());
+        System.out.println(node2.isLast());
+
     }
 
 }
